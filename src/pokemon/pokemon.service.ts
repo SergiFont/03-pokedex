7611bc5254775d06fb1ba887b79609109becc9ea
SergiFont <sfontsmx2010@gmail.com>
2023-04-18 15:56:58 +0200

    study 18/04/23. Finished section 8

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,6 +5,7 @@ import { BadRequestException, Injectable, InternalServerErrorException, NotFound
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -27,8 +28,17 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+
+    const { limit = 10, offset = 0} = paginationDto
+
+    return this.pokemonModel.find()
+      .limit(limit)
+      .skip(offset)
+      .sort({
+        no: 1
+      })
+      .select('-__v') // elimino de los resultados la columna '__v'
   }
 
   async findOne(term: string) {
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,52 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios // dependencia no inyectada
+  constructor(
+    @InjectModel( Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>, // dependencia inyectada
+    private readonly http: AxiosAdapter,
+  ) {}
 
-  
+
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-    data.results.forEach(( { name, url}) => {
+    await this.pokemonModel.deleteMany( {})
+
+    const data: PokeResponse = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+
+    // const insertPromisesArray = []
+    const pokemonToInsert: {name: string, no: number}[] = []
+
+    data.results.forEach(( { name, url } ) => {
       
       const segments = url.split('/'); // fragmento la url en segmentos
       const no: number = +segments[ segments.length - 2 ] // extraigo el número en la penúltima posición de la url fragmentada
 
-      console.log({name, no});
+      // insertPromisesArray.push(
+      //   this.pokemonModel.create({no, name})
+      // ) 
+      /**
+       * En la primera opción, se creaba un array de promesas con todas ellas sin resolver. Luego fuera del for each, se resolvian
+       * con el Promise.all. De esta manera se insertan de uno en uno en la base de datos.
+       * En la segunda opción (la aplicada) se crea un array de objetos con los datos necesarios de cada pokemon. De esta otra
+       * manera se insertan todos de golpe en la base de datos. Mucho mas eficiente.
+       */
+      pokemonToInsert.push( {name, no} )
+
     })
 
-    return data.results;
+    // await Promise.all( insertPromisesArray );
+    this.pokemonModel.insertMany(pokemonToInsert)
+
+    return 'Seed executed'
   }
 }
